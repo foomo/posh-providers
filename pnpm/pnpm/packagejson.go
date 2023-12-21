e78b2f667f3a62c8230a0e79e5a5c60cdf9755a9
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2023-12-21 14:41:25 +0100

    feat: add pnpm command

---
+++ pnpm/pnpm/packagejson.go
@@ -0,0 +1,39 @@
+package pnpm
+
+import (
+	"encoding/json"
+	"os"
+
+	"github.com/pkg/errors"
+)
+
+// PackageJSON represents NodeJS package.json
+type PackageJSON struct {
+	Name        string            `json:"name"`
+	Version     string            `json:"version"`
+	Description string            `json:"description"`
+	Keywords    []string          `json:"keywords"`
+	Homepage    string            `json:"homepage"`
+	License     string            `json:"license"`
+	Files       []string          `json:"files"`
+	Main        string            `json:"main"`
+	Scripts     map[string]string `json:"scripts"`
+	Os          []string          `json:"os"`
+	Cpu         []string          `json:"cpu"`
+	Private     bool              `json:"private"`
+}
+
+func LoadPackageJSON(filename string) (*PackageJSON, error) {
+	w := &PackageJSON{}
+
+	file, err := os.ReadFile(filename)
+	if err != nil {
+		return nil, errors.Wrap(err, "failed to read file")
+	}
+
+	if err = json.Unmarshal(file, w); err != nil {
+		return nil, errors.Wrap(err, "failed to unmarshal")
+	}
+
+	return w, nil
+}
