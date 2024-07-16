95a3bdf1798de91a0a47889e03bb5be77d732a6f
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2024-07-16 22:49:31 +0200

    feat(gruntwork-io/terragrunt): add provider

---
+++ gruntwork-io/terragrunt/config.go
@@ -0,0 +1,74 @@
+package terragrunt
+
+import (
+	"context"
+	"os"
+	"path"
+	"strings"
+
+	"github.com/foomo/posh/pkg/util/files"
+	"golang.org/x/exp/slices"
+)
+
+type Config struct {
+	Path      string `json:"path" yaml:"path"`
+	CachePath string `json:"cachePath" yaml:"cachePath"`
+}
+
+// ------------------------------------------------------------------------------------------------
+// ~ Public methods
+// ------------------------------------------------------------------------------------------------
+
+func (c Config) EnvNames() []string {
+	return c.dirNames(c.EnvsPath())
+}
+
+func (c Config) EnvsPath() string {
+	return path.Join(c.Path, "envs")
+}
+
+func (c Config) SiteNames(env string) []string {
+	return c.dirNames(c.SitesPath(env))
+}
+
+func (c Config) SitesPath(env string) string {
+	return path.Join(c.EnvsPath(), env)
+}
+
+func (c Config) StackNames(ctx context.Context, env, site string) ([]string, error) {
+	var ret []string
+	root := c.StacksPath(env, site)
+	out, err := files.Find(ctx, root, "terragrunt.hcl", files.FindWithIsFile(true))
+	if err != nil {
+		return nil, err
+	}
+	for _, v := range out {
+		v = strings.TrimPrefix(v, root)
+		v = strings.TrimSuffix(v, "/terragrunt.hcl")
+		if v = strings.TrimPrefix(v, "/"); strings.Contains(v, "/") {
+			ret = append(ret, v)
+		}
+	}
+	slices.Sort(ret)
+	return ret, err
+}
+
+func (c Config) StacksPath(env, site string) string {
+	return path.Join(c.SitesPath(env), site)
+}
+
+// ------------------------------------------------------------------------------------------------
+// ~ Private methods
+// ------------------------------------------------------------------------------------------------
+
+func (c Config) dirNames(path string) []string {
+	var ret []string
+	if files, err := os.ReadDir(path); err == nil {
+		for _, value := range files {
+			if value.IsDir() && !strings.HasPrefix(value.Name(), ".") && !strings.HasPrefix(value.Name(), "_") {
+				ret = append(ret, value.Name())
+			}
+		}
+	}
+	return ret
+}
