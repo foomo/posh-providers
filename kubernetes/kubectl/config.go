2aa1019ee70cf120715ed1fe7183784ee62fd9e3
Kevin Franklin Kim <kevin.kim@bestbytes.com>
2026-07-20 23:12:37 +0200

    feat: cleanup

--- kubernetes/kubectl/config.go
+++ kubernetes/kubectl/config.go
@@ -1,18 +1,6 @@
 package kubectl
 
-type ClusterConfig struct {
-	Proxy string `json:"proxy" yaml:"proxy" jsonschema:"description=Named SSH proxy from the top-level proxies map"`
-}
-
 type Config struct {
-	ConfigPath string                   `json:"configPath" yaml:"configPath"`
-	Clusters   map[string]ClusterConfig `json:"clusters"   yaml:"clusters"`
-}
-
-func (c Config) ClusterProxy(name string) string {
-	if cc, ok := c.Clusters[name]; ok {
-		return cc.Proxy
-	}
-
-	return ""
+	// ConfigPath to store kubeconfigs
+	ConfigPath string `json:"configPath" yaml:"configPath"`
 }
